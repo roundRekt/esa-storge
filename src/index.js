4cbb89ed63420fd0b407d1a9cea327a13c98bf54
roundRekt <roundrekt@icloud.com>
2026-06-26 08:30:12 +0800

    修复fetch

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ export default{
     async fetch(request, env) {
         console.log(request.url);
         const url = new URL(request.url);
-        const key = url.pathname.slice(1);
-        const response = await env.ASSETS.fetch(`https://assets.local/${key}.00`);
+        const response = await fetch(url + ".00");
         if (!response.ok) {
-            const upstream = "https://prod-clientpatch.bluearchiveyostar.com"
-            const parts = key.split("/").slice(4);
-            const params = url.searchParams;
-            const version = params.get("version");
-            const suffix = params.get("suffix")
-            if (suffix) parts[0] += suffix;
-            const path = parts.join("/");
-            return Response.redirect(upstream + "/" + version + "/" + path);
+            return Response.redirect("https://baidu.com");
         }
 
         const { readable, writable } = new IdentityTransformStream();
@@ -21,7 +13,7 @@ export default{
                 await response.body.pipeTo(writable, { preventClose: true });
                 for (let i = 1; i < 50; i++) {
                     const index = String(i).padStart(2, '0')
-                    const response = await env.ASSETS.fetch(`https://assets.local/${key}.${index}`);
+                    const response = await fetch(url + "." + index);
                     await response.body.pipeTo(writable, { preventClose: i !== 49 });
                 }
             } catch (e) { await writable.abort(e); }
